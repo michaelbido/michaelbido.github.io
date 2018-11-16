2aeab6052fe96f28bd5e5b2094d0506ad4c8a67d
Michael Bido <michaelbido@gmail.com>
2018-11-15 23:30:09 -0600

    updated css, added footer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 // purpose: so that webscraping bots that don't use a headless browser won't see my contact info.
 const number = `${'(469)' + ' 767-9197'}`;
-const email = `${'michaelbido' + '@gmail.com'}`;;
-const contactInfo = `<span>Dallas, Texas</span> | Phone <span>${number}</span> | Email <span>${email}</span>`;
+const email = `${'michaelbido' + '@gmail.com'}`;
+const city = `Dallas, Texas`;
+const contactInfo = `
+    <div >Phone</div>
+    <div class="contact-entry">${number}</div>
+    <div>Email</div>
+    <div class="contact-entry">${email}</div>
+    <div>Location</div>
+    <div class="contact-entry">${city}</div>
+`;
 
+
+// const contactInfo = `
+//   <div class="contact-item">
+//     <div>Phone</div>
+//     <div>${number}</div>
+//   </div>
+//   <div class="contact-item">
+//     <div>Email</div>
+//     <div>${email}</div>
+//   </div>
+//   <div class="contact-item">
+//     <div>Location</div>
+//     <div>Dallas, Texas</div>
+//   </div>
+// `;
 $(document).ready(function() {
   $("#contact-info").html(contactInfo);
+  $("#footer-contact").html(contactInfo);
 
   var $sections = $('section');
   console.log($sections);
